ebe3dd9dacd422ac0943b470963c87b80abaab8f
Amrendra Kumar <amrendra.nitb@gmail.com>
2016-04-06 02:13:57 +0530

    Remove button & un-necessary views in splashscreen activity

--- app/src/main/java/com/amrendra/codefiesta/ui/activities/SplashScreenActivity.java
+++ app/src/main/java/com/amrendra/codefiesta/ui/activities/SplashScreenActivity.java
@@ -1,12 +1,12 @@
 package com.amrendra.codefiesta.ui.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPropertyAnimatorCompat;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.amrendra.codefiesta.R;
@@ -21,7 +21,6 @@ public class SplashScreenActivity extends BaseActivity {
     public static final int ANIM_ITEM_DURATION = 1000;
     public static final int ITEM_DELAY = 250;
     public static final int ITEM_DELAY_OFFSET = 400;
-    public static final int ANIM_BUTTON_DURATION = 500;
     public static final int ANIM_TEXT_DURATION = 1000;
     public static final int LOGO_Y_TRANSLATION = -150;
     public static final int TEXT_Y_TRANSLATION = 50;
@@ -36,9 +35,13 @@ public class SplashScreenActivity extends BaseActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
         setContentView(R.layout.splash_activity);
-/*        Intent intent = new Intent(this, ListActivity.class);
+
+    }
+
+    private void startMainListActivity() {
+        Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
-        finish();*/
+        finish();
     }
 
 
@@ -64,21 +67,22 @@ public class SplashScreenActivity extends BaseActivity {
 
         for (int i = 0; i < container.getChildCount(); i++) {
             View v = container.getChildAt(i);
-            ViewPropertyAnimatorCompat viewAnimator;
+            ViewPropertyAnimatorCompat viewAnimator = ViewCompat.animate(v)
+                    .translationY(TEXT_Y_TRANSLATION).alpha(1)
+                    .setStartDelay((ITEM_DELAY * i) + ITEM_DELAY_OFFSET)
+                    .setDuration(ANIM_TEXT_DURATION);
 
-            if (!(v instanceof Button)) {
-                viewAnimator = ViewCompat.animate(v)
-                        .translationY(TEXT_Y_TRANSLATION).alpha(1)
-                        .setStartDelay((ITEM_DELAY * i) + ITEM_DELAY_OFFSET)
-                        .setDuration(ANIM_TEXT_DURATION);
-            } else {
-                viewAnimator = ViewCompat.animate(v)
-                        .scaleY(1).scaleX(1)
-                        .setStartDelay((ITEM_DELAY * i) + ITEM_DELAY_OFFSET)
-                        .setDuration(ANIM_BUTTON_DURATION);
+            if (v.getId() == R.id.hello_world_tv) {
+                viewAnimator.withEndAction(runnable);
             }
-
             viewAnimator.setInterpolator(new DecelerateInterpolator()).start();
         }
     }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            startMainListActivity();
+        }
+    };
 }
